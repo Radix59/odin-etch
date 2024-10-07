92ccaf3c87e76c3d373cffda57eafefee3fb2efc
Alex <alex@moudry.net>
2024-10-07 14:43:37 -0500

    Add grid reset function and inputs

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
 const boxes = [];
 const gridContainer = document.querySelector(".grid-container");
+let gridSize = 16;
 
-for (let i = 0; i < 256; i++)
+function generateCanvas(gridSize)
 {
-    boxes[i] = document.createElement("div");
-    boxes[i].className = "gridbox";
-    boxes[i].id = "box-" + (i + 1);
-    gridContainer.appendChild(boxes[i]);
+    const numOfPixels = gridSize * gridSize;
+    const pixelWidth = 100 / (gridSize);
+    for (let i = 0; i < numOfPixels; i++)
+    {
+        boxes[i] = document.createElement("div");
+        boxes[i].className = "pixel";
+        boxes[i].id = "box-" + (i + 1);
+        boxes[i].style.width = pixelWidth + "%"
+        gridContainer.appendChild(boxes[i]);
+    }
+}
+
+function clearCanvas()
+{
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(element =>
+    {
+        gridContainer.removeChild(element);
+    });
+}
+
+function userResetCanvas()
+{
+    const userGridSize = prompt("Enter the number of pixels per side");
+    if (isNaN(userGridSize) || !Number.isInteger(+userGridSize))
+    {
+        alert("Invalid! Not an integer!");
+        return;
+    }
+    if (+userGridSize > 100)
+    {
+        alert("Too big! Please input a number < 100");
+        return;
+    }
+    clearCanvas();
+    generateCanvas(+userGridSize);
+    return;
 }
 
 gridContainer.addEventListener("mouseover", (event) =>
 {
     let target = event.target;
-    if (target.className === "gridbox")
-        target.style.backgroundColor = "purple";
+    if (target.className === "pixel")
+    {
+        if (target.style.backgroundColor === "")
+            target.style.backgroundColor = "purple";
+        //else if (target.style.backgroundColor === "purple")
+        //    target.style.backgroundColor = "";
+    }
 });
 
-gridContainer.addEventListener("mouseout", (event) =>
-{
-    let target = event.target;
-    if (target.className === "gridbox")
-        target.style.backgroundColor = "";
-});
+generateCanvas(gridSize);
 
