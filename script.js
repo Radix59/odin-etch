04d155a35ee40f4ee272c0bdfd473cb7099e6b5a
Alex <alex@moudry.net>
2024-10-07 12:32:39 -0500

    Added working 16x16 grid with hover events

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const boxes = [];
 const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 0; i < 16; i++)
+for (let i = 0; i < 256; i++)
 {
     boxes[i] = document.createElement("div");
     boxes[i].className = "gridbox";
     boxes[i].id = "box-" + (i + 1);
     gridContainer.appendChild(boxes[i]);
 }
+
+gridContainer.addEventListener("mouseover", (event) =>
+{
+    let target = event.target;
+    if (target.className === "gridbox")
+        target.style.backgroundColor = "purple";
+});
+
+gridContainer.addEventListener("mouseout", (event) =>
+{
+    let target = event.target;
+    if (target.className === "gridbox")
+        target.style.backgroundColor = "";
+});
+
